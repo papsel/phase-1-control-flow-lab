3a6b9e9a37f726b49fb8a3995110ae2f0336b5b6
papsel <elijah.njora@student.moringaschool.com>
2023-06-20 04:49:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance<=400){
+    return 'This one is on me!';
+  } else if(distance > 2000 && distance <= 2500){
+    return 'I will gladly take your thirty bucks.';
+  }else if (distance>2500){
+    return 'No one can do';
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  return city === 'NYC'? 'ok sounds goog': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+  case 'generous':
+    return 'Thank you so much.';
+    case 'not generous.':
+      return 'Thank you.';
+      default:
+        return 'Bye.'
+}
 }
